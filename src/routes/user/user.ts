0ec0526eb906316786162f907a6e3248f922282c
AVANADE-CORP\h.luiz.mattos.silva <h.luiz.mattos.silva@avanade.com>
2021-10-25 09:13:10 -0300

    teste

--- src/routes/user/user.ts
+++ src/routes/user/user.ts
@@ -4,7 +4,7 @@ import { http } from 'src/http';
 
 const user = Router();
 
-user.get('/', async (req: Request, res: Response) => {
+user.get('/teste', async (req: Request, res: Response) => {
   const { data } = await http.get<AxiosResponse>('/posts');
 
   res.status(200).json({
